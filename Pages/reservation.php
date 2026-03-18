e1da3ac311d1d01ceb4ba220ae886576ae05f484
Miya <miyakouadio@yahoo.com>
2026-03-18 10:59:51 +0100

    test1

--- Pages/reservation.php
+++ Pages/reservation.php
@@ -1,8 +1,6 @@
-
-<?php error_reporting(E_ALL);
-ini_set("display_errors", 1);
+<?php 
+include_once('Config/config.php');
 ?>
- 
     <div>
         <form action="" method="post" enctype="multipart/form-data" id="reservationForm">
             <label for="service">service:</label>
