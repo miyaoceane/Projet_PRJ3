4937712f7d9a274a60de2e5327bdd65437c32889
06Laetitia <108051@ecole-it.com>
2026-03-18 09:18:32 +0100

    debut code P2

--- INC/header.inc.php
+++ INC/header.inc.php
@@ -3,8 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>iplay</title>
 </head>
 <body>
+
+    <header>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
     
-    
+    <!-- Nom du salon de coiffure -->
+    <a class="navbar-brand" href="#">Nom du salon</a>
+
+    <!-- Menu -->
+    <div class="d-flex align-items-center gap-3">
+      <a class="nav-link" href="#">Contact</a>
+      <a href="reservation.php" class="btn btn-outline-success">Réserver</a>
+    </div>
+
+  </div>
+</nav>
+</header>
+    <section>
+        <div class="container">
